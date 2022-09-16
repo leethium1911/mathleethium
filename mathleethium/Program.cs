01077b2d77cc96f79b7a46f8e7b17948ba1dad2f
VLllll VLllll <leevalera92@gmail.com>
2022-09-16 13:02:24 +0400

    Добавьте файлы проекта.

---
+++ mathleethium/Program.cs
@@ -0,0 +1,45 @@
+﻿    using System;
+
+namespace mathleethium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            Random z = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                a[i] = z.Next(0, 1001);
+                Console.Write("{0,2} ", a[i]);
+            }
+            Console.WriteLine();
+            int min = a[0];
+            for (int i = 1; i < 100; i++)
+                min = Math.Min(min, a[i]);
+            
+
+            int max = a[0];
+            for (int i = 1; i < 100; i++)
+                max = Math.Max(max, a[i]);
+
+            Console.WriteLine("Показать максимальное значение - Макс");
+            Console.WriteLine("Показать минимальное значение - Мин");
+
+            string lang = Console.ReadLine();
+            switch (lang)
+            {
+                case "Макс":
+                    Console.WriteLine(+ max + " - Максимальное число");
+                    break;
+                case "Мин":
+                    Console.WriteLine(+ min + " - Минимальное число");
+                    break;
+
+
+            }
+
+
+        }
+    }
+}
