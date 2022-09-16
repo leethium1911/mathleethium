579fcfc53c156fbb7ff3d335de08699febcd36c1
VLllll VLllll <leevalera92@gmail.com>
2022-09-16 13:30:17 +0400

    Ceiling, Floor

--- mathleethium/Program.cs
+++ mathleethium/Program.cs
@@ -6,38 +6,49 @@ namespace mathleethium
     {
         static void Main(string[] args)
         {
-            int[] a = new int[100];
-            Random z = new Random();
-            for (int i = 0; i < 100; i++)
-            {
-                a[i] = z.Next(0, 1001);
-                Console.Write("{0,2} ", a[i]);
-            }
-            Console.WriteLine();
-            int min = a[0];
-            for (int i = 1; i < 100; i++)
-                min = Math.Min(min, a[i]);
-            
-
-            int max = a[0];
-            for (int i = 1; i < 100; i++)
-                max = Math.Max(max, a[i]);
-
-            Console.WriteLine("Показать максимальное значение - Макс");
-            Console.WriteLine("Показать минимальное значение - Мин");
-
-            string lang = Console.ReadLine();
-            switch (lang)
-            {
-                case "Макс":
-                    Console.WriteLine(+ max + " - Максимальное число");
-                    break;
-                case "Мин":
-                    Console.WriteLine(+ min + " - Минимальное число");
-                    break;
+            //int[] a = new int[100];
+            //Random z = new Random();
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    a[i] = z.Next(0, 1001);
+            //    Console.Write("{0,2} ", a[i]);
+            //}
+            //Console.WriteLine();
+            //int min = a[0];
+            //for (int i = 1; i < 100; i++)
+            //    min = Math.Min(min, a[i]);
 
 
-            }
+            //int max = a[0];
+            //for (int i = 1; i < 100; i++)
+            //    max = Math.Max(max, a[i]);
+
+            //Console.WriteLine("Показать максимальное значение - Макс");
+            //Console.WriteLine("Показать минимальное значение - Мин");
+
+            //string lang = Console.ReadLine();
+            //switch (lang)
+            //{
+            //    case "Макс":
+            //        Console.WriteLine(+ max + " - Максимальное число");
+            //        break;
+            //    case "Мин":
+            //        Console.WriteLine(+ min + " - Минимальное число");
+            //        break;
+
+
+            //}
+            Console.WriteLine("Округление до целого числа");
+
+            double v = double.Parse(Console.ReadLine());           
+            Console.WriteLine("Ближайшее целое число в большую сторону:  " + Math.Ceiling(v));
+            Console.WriteLine("Ближайшее целое число в меньшую:    " + Math.Floor(v));
+
+            Console.ReadKey();
+
+
+
+
 
 
         }
